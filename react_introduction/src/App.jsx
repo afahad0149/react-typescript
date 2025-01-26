0e85b52c46f7dcec3eea520d987d8f4f28d66d6d
Akib Ahmed Fahad <afahad0149@Akibs-MacBook-Pro.local>
2025-01-26 12:08:37 +0600

    learned values that can be rendered

---
+++ react_introduction/src/App.jsx
@@ -0,0 +1,12 @@
+import {Greetings} from "./Greetings";
+
+export function App() {
+    return (
+        <>
+            <input type="text" name="" id=""/>
+            <input type="text" name="" id=""/>
+            <Greetings/>
+        </>
+
+    );
+}
---
+++ react_introduction/src/Greetings.jsx
@@ -0,0 +1,11 @@
+export function Greetings() {
+    return (
+        <>
+            <ul>
+                <li>Hello</li>
+                <li>World!</li>
+                <li>How ya?</li>
+            </ul>
+        </>
+    )
+}
